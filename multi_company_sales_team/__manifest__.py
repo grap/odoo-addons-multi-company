928d6ca7a9cf837dfb23347c4b2b48fe0484336b
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-08-06 11:31:50 +0200

    [ADD] multi_company_sales_team module

--- multi_company_sales_team/__manifest__.py
+++ multi_company_sales_team/__manifest__.py
@@ -4,19 +4,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Multi Company - Sale Module',
+    'name': 'Multi Company - Sales Team Module',
     'version': '10.0.1.0.0',
     'category': 'Multi Company',
-    'summary': 'Handle Multi company for Sale Module',
+    'summary': 'Handle Multi company for Sales Team Module',
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
     'license': 'AGPL-3',
     'depends': [
         'multi_company_base',
-        'sale',
+        'sales_team',
     ],
     'data': [
-        'views/view_sale_order.xml',
+        'views/view_crm_team.xml',
     ],
     'installable': True,
 }
