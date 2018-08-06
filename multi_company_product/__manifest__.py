e35f30f4cc2c3e0365365a2df9f54b9ec5196451
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-08-06 11:18:33 +0200

    [ADD] multi_company_product module

---
+++ multi_company_product/__manifest__.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+# Copyright (C) 2014-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Multi Company - Product Module',
+    'version': '10.0.1.0.0',
+    'category': 'Multi Company',
+    'summary': 'Handle Multi company for Product Module',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'multi_company_base',
+        'product',
+    ],
+    'data': [
+        'views/view_product_template.xml',
+        'views/view_product_pricelist.xml',
+        'views/view_product_pricelist_item.xml',
+    ],
+    'demo': [
+        'demo/res_groups.xml',
+    ],
+    'installable': True,
+}
